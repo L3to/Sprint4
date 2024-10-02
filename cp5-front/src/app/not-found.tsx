1a9b55403b008bf0a62e7389508b0fa5339f776e
L3to <lf2006@hotmail.com.br>
2024-10-02 19:19:47 -0300

    Not-found criada

---
+++ cp5-front/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function NotFound() {
+    
+  return (
+    <div>
+        <h1>404: Page Not Found</h1>
+        <p>O conteúdo que você está tentando acessar não está disponível!</p>
+        <Image src="/img/mouse.jpg" alt="mouse" width={50} height={50} priority={true} />
+    </div>
+  )
+}   
