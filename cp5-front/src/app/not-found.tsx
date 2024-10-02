c18c49ac78600bb0a01c141f0a3513b4f33d21cc
L3to <lf2006@hotmail.com.br>
2024-10-02 19:29:09 -0300

    Not-Found atualizado

--- cp5-front/src/app/not-found.tsx
+++ cp5-front/src/app/not-found.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function NotFound() {
-    
+
   return (
     <div>
-        <h1>404: Page Not Found</h1>
-        <p>O conteúdo que você está tentando acessar não está disponível!</p>
-        <Image src="/img/mouse.jpg" alt="mouse" width={50} height={50} priority={true} />
+      <h1>Oh...</h1>
+      <p>Parece que o conteúdo que você está tentando acessar não está disponível!</p>
+      <Image src="/img/capalivroimagem404" alt="Capa do livro Worlds in Collision" width={50} height={50} priority={true} />
+      <Link href={"/"}>Voltar á página inicial</Link>
     </div>
   )
 }   
