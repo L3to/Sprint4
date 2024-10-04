4f8fae6ceaa776c1f5194b5e5ebc11a6a419eb68
L3to <lf2006@hotmail.com.br>
2024-10-04 19:18:28 -0300

    not-found atualizado

--- cp5-front/src/app/not-found.tsx
+++ cp5-front/src/app/not-found.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
-
   return (
-    <div className="flex flex-col items-center justify-center text-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">Oh...</h1>
-      <p className="text-lg mb-4">Parece que o conteúdo que você está tentando acessar não está disponível!</p>
-      <div className="w-12 h-12 mb-4">
-        <Image src="/img/source.gif" alt="Saturnodestruído" layout="responsive" width={50} height={50} priority={true} />
+    <div className="flex flex-col md:flex-row items-center justify-center text-center md:text-left min-h-screen p-4">
+      <div className="w-160 h-160 md:w-160 md:h-160 mb-8 md:mb-0 md:mr-8">
+        <Image src="/img/source.gif" alt="Saturno destruído" layout="responsive" width={200} height={200} priority={true} />
       </div>
-      <Link href="/"> 
-        <p className="px-4 py-2 bg-red-700 text-white rounded hover:bg-red-900 transition">Voltar á página inicial</p>
-      </Link>
+      <div>
+        <h1 className="text-4xl md:text-8xl font-bold mb-4">Oh...</h1>
+        <p className="text-xl md:text-4xl mb-4">Parece que o conteúdo que você está tentando acessar não está disponível!</p>
+        <Link href="/">
+          <p className="px-4 py-2 bg-red-700 text-white text-xl md:text-2xl rounded hover:bg-red-900 transition">Voltar á página inicial</p>
+        </Link>
+      </div>
     </div>
-  )
-}   
+  );
+}
